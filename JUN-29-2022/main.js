d44a0e2eed06c04172b62275e55a6af881393670
Jose <jnscsilvacastellon@gmail.com>
2022-07-01 12:59:40 -0700

    Kata of the day Jun 29 2022

---
+++ JUN-29-2022/main.js
@@ -0,0 +1,15 @@
+/*
+
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+123 --> "123"
+999 --> "999"
+
+*/
+
+function numberToString(number) {
+    return number.toString(); // number + "" ===>>> another way to do it
+  }
